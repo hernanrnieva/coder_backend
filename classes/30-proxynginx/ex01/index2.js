cb7e187a201acde7f36cb0475a037fc7dd19965f
hernanrnieva <hnieva@fi.uba.ar>
2022-06-09 00:15:43 -0300

    added final files for class 30

---
+++ classes/30-proxynginx/ex01/index2.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+
+const PORT = parseInt(process.argv[3]) || 8081
+
+app.get('/data', (req, res) => {
+    console.log(`Port: ${PORT} -> Fyh: ${Date.now()}`)
+    res.send(`Server express <span style="color:blueviolet;">(Nginx)</span> at port: ${PORT} - <b>PID: ${process.pid}</b> - ${new Date().toLocaleString()}`)
+})
+
+app.listen(PORT, err => {
+    if(!err)
+        console.log(`Server up running at port ${PORT}`)
+})
